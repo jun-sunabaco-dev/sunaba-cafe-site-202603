cfe1599590d8a1e32b2526b5b8b6dd490152e50b
jun-sunabaco-dev <xt226sm4798hku@gmail.com>
2026-03-20 21:06:13 +0900

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,24 @@ const navToggle = document.querySelector('.nav-toggle');
 const siteNav = document.querySelector('.site-nav');
 const navLinks = document.querySelectorAll('.site-nav a');
 
+const heroImg = document.querySelector('.hero-image img');
+
+if (heroImg) {
+  heroImg.style.opacity = 0;
+
+  setTimeout(() => {
+    const hour = new Date().getHours();
+
+    if (hour >= 6 && hour < 18) {
+      heroImg.src = './assets/hero.png';
+    } else {
+      heroImg.src = './assets/hero_Night.png';
+    }
+
+    heroImg.style.opacity = 1;
+  }, 200);
+}
+
 if (navToggle && siteNav) {
   navToggle.addEventListener('click', () => {
     const isOpen = siteNav.classList.toggle('is-open');
